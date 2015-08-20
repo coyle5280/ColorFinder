6c98053d399cfe84ffe3f290787b38b7e0876d92
Joshua Coyle <coyle5280@gmail.com>
2015-08-19 23:34:25 -0600

    cleaned up code

--- ColorFinder/src/Color/Finder/Assignment/MainActivity.java
+++ ColorFinder/src/Color/Finder/Assignment/MainActivity.java
@@ -31,6 +31,9 @@ public class MainActivity extends Activity {
 
     }
 
+    /**
+     *
+     */
     private void setupNumberPickers() {
         red = (NumberPicker) findViewById(R.id.redPicker);
         green = (NumberPicker) findViewById(R.id.greenPicker);
@@ -48,56 +51,53 @@ public class MainActivity extends Activity {
         green.setMinValue(min);
         blue.setMinValue(min);
 
+        //Set Start Value
+        red.setValue(255);
+        green.setValue(255);
+        blue.setValue(255);
+
+        theCombinedColor = Color.rgb(red.getValue(),
+                green.getValue(), blue.getValue());
+        setTextViewColor(theCombinedColor);
+
+        // Listener for the NumberPicker Red
         red.setOnValueChangedListener(new OnValueChangeListener() {
             @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                int tempRed;
-                int tempBlue;
-                int tempGreen;
-
-                tempRed = red.getValue();
-                tempBlue = blue.getValue();
-                tempGreen = green.getValue();
-
-                theCombinedColor = Color.rgb(tempRed, tempGreen, tempBlue);
+            public void onValueChange(NumberPicker picker, int oldVal,
+                    int newVal) {
+                theCombinedColor = Color.rgb(red.getValue(),
+                        green.getValue(), blue.getValue());
                 setTextViewColor(theCombinedColor);
 
             }
         });
-
+        // Listener for the NumberPicker Blue
         blue.setOnValueChangedListener(new OnValueChangeListener() {
             @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                int tempRed;
-                int tempBlue;
-                int tempGreen;
-
-                tempRed = red.getValue();
-                tempBlue = blue.getValue();
-                tempGreen = green.getValue();
-
-                theCombinedColor = Color.rgb(tempRed, tempGreen, tempBlue);
+            public void onValueChange(NumberPicker picker, int oldVal,
+                    int newVal) {
+                theCombinedColor = Color.rgb(red.getValue(),
+                        green.getValue(), blue.getValue());
                 setTextViewColor(theCombinedColor);
             }
         });
 
+        // Listener for the NumberPicker Green
         green.setOnValueChangedListener(new OnValueChangeListener() {
             @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                int tempRed;
-                int tempBlue;
-                int tempGreen;
-
-                tempRed = red.getValue();
-                tempBlue = blue.getValue();
-                tempGreen = green.getValue();
-
-                theCombinedColor = Color.rgb(tempRed, tempGreen, tempBlue);
+            public void onValueChange(NumberPicker picker, int oldVal,
+                    int newVal) {
+                theCombinedColor = Color.rgb(red.getValue(),
+                        green.getValue(), blue.getValue());
                 setTextViewColor(theCombinedColor);
             }
         });
     }
 
+    /**
+     *
+     * @param colorToSet
+     */
     public void setTextViewColor(int colorToSet) {
         theColorView.setBackgroundColor(colorToSet);
     }
